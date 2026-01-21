8d17dd2519651b9c87a7775d982ea5e502fddaf5
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2026-01-21 13:57:53 +0900

    base item clasw

---
+++ envs/items/item.py
@@ -0,0 +1,9 @@
+
+
+class Item:
+    def __init__(self, name: str):
+        self.name = name
+        self.price: float = 0.0
+
+    def __repr__(self):
+        return f"Item(name={self.name}, price={self.price})"
