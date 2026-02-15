4fb4b39f32ec084c7595c5b24881e1c7da78a9e8
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2026-02-15 16:47:14 +0900

    change test to support random config

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,21 +1,16 @@
+import copy
 import random
 from econsimulacra.agents import Agent
-from econsimulacra.envs import JsonRandom
 import pytest
+from typing import Any
 
 
 class DummyAgent(Agent):
-    def _initialize_inventory(self, config) -> dict[str, float | int]:
-        return {"cash": 100, "rice": 50}
-
     def act(self, obs):
         pass
 
 
 class DummyAgentwName(Agent):
-    def _initialize_inventory(self, config) -> dict[str, float | int]:
-        return {"cash": 100, "rice": 50}
-
     def self_assign_name(self, config) -> None:
         self.agent_name = f"DummyAgent{self.agent_id}"
 
@@ -23,55 +18,50 @@ class DummyAgentwName(Agent):
         pass
 
 
-class DummyAgentwRandomInventory(Agent):
-    def _initialize_inventory(self, config) -> dict[str, float | int]:
-        json_random = JsonRandom(prng=self.prng)
-        cash_amount = json_random.random(json_value=config["cashAmount"])
-        rice_amount = json_random.random(json_value=config["riceAmount"])
-        return {"cash": cash_amount, "rice": rice_amount}
-
-    def act(self, obs):
-        pass
-
-
 class TestAgent:
+    agent_config: dict[str, Any] = {
+        "inventory": {
+            "cash": 100,
+            "rice": 50,
+        }
+    }
     def test__init__(self) -> None:
-        agent = DummyAgent(agent_id=1, agent_name="TestAgent")
+        agent = DummyAgent(agent_id=1, agent_name="TestAgent", config=self.agent_config)
         assert agent.agent_id == 1
         assert agent.agent_type == "DummyAgent"
         assert agent.agent_name == "TestAgent"
         assert agent.is_rich_info_allowed == False
         assert agent.inventory_dic == {"cash": 100, "rice": 50}
-        agent = DummyAgentwName(agent_id=2, agent_name="IgnoredName")
+        agent = DummyAgentwName(agent_id=2, agent_name="IgnoredName", config=self.agent_config)
         assert agent.agent_id == 2
         assert agent.agent_type == "DummyAgentwName"
         assert agent.agent_name == "DummyAgent2"
-        agent = DummyAgentwRandomInventory(
+        random_config = copy.deepcopy(self.agent_config)
+        random_config["inventory"]["cash"] = [50, 100]
+        random_config["inventory"]["rice"] = [30, 70]
+        random_config["isRichInfoAllowed"] = True
+        agent = DummyAgent(
             agent_id=3,
             agent_name="TestAgent3",
             prng=random.Random(42),
-            config={
-                "cashAmount": [50, 100],
-                "riceAmount": [30,70],
-                "isRichInfoAllowed": True,
-            },
+            config=random_config,
         )
         assert agent.agent_id == 3
-        assert agent.agent_type == "DummyAgentwRandomInventory"
+        assert agent.agent_type == "DummyAgent"
         assert agent.agent_name == "TestAgent3"
         assert agent.is_rich_info_allowed == True
         cash_amount = agent.inventory_dic["cash"]
         rice_amount = agent.inventory_dic["rice"]
         assert 50 <= cash_amount <= 100
         assert 30 <= rice_amount <= 70
-        agent = DummyAgentwRandomInventory(
+        random_config = copy.deepcopy(self.agent_config)
+        random_config["inventory"]["cash"] = [101, 150]
+        random_config["inventory"]["rice"] = [71, 110]
+        agent = DummyAgent(
             agent_id=4,
             agent_name="TestAgent3",
             prng=random.Random(42),
-            config={
-                "cashAmount": [101, 150],
-                "riceAmount": [71,110]
-            },
+            config=random_config,
         )
         assert agent.agent_id == 4
         assert agent.agent_name == "TestAgent3"
@@ -81,7 +71,7 @@ class TestAgent:
         assert 71 <= rice_amount <= 110
 
     def test_exchange_goods(self) -> None:
-        agent = DummyAgent(agent_id=1, agent_name="TestAgent")
+        agent = DummyAgent(agent_id=1, agent_name="TestAgent", config=self.agent_config)
         agent.exchange_goods(
             get_item_name="cash",
             get_item_amount=20,
