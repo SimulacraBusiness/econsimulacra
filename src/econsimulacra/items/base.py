59ebea5b254426454f6959fd62b33164e2207dd3
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2026-02-15 15:59:30 +0900

    add set_price, and add set_by in set_price

--- src/econsimulacra/items/base.py
+++ src/econsimulacra/items/base.py
@@ -1,12 +1,19 @@
+from typing import Optional
+
+
 class Item:
     def __init__(self, item_id: int, item_name: str):
         self.item_id: int = item_id
         self.item_name: str = item_name
         self.price: float = 0.0
+        self.price_set_by: Optional[int] = None
 
-    def set_price(self, price: float) -> None:
-        """Set the price. Often used by retailers to set the price of items they sell."""
+    def get_price(self) -> float:
+        return self.price
+
+    def set_price(self, price: float, set_by: Optional[int] = None) -> None:
         self.price = price
+        self.price_set_by = set_by
 
     def __repr__(self):
-        return f"Item(id={self.item_id}, name={self.item_name}, price={self.price})"
+        return f"Item(id={self.item_id}, name={self.item_name}, price={self.price}, price_set_by={self.price_set_by})"
