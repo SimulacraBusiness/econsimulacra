bbbdf8fa7744b38afba66fffc1b24f986a905601
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-02-28 10:56:56 +0900

    add persona builder

--- src/econsimulacra/llm_services/__init__.py
+++ src/econsimulacra/llm_services/__init__.py
@@ -4,12 +4,16 @@ from .clients import TransformersClient as TransformersClient
 from .constant import DEFAULT_ACTION_JSON_SCHEMA as DEFAULT_ACTION_JSON_SCHEMA
 from .constant import DEFAULT_ACTION_DESCRIPTION as DEFAULT_ACTION_DESCRIPTION
 from .constant import DEFAULT_OBS_DESCRIPTION as DEFAULT_OBS_DESCRIPTION
+from .personas import PersonaBuilder as PersonaBuilder
+from .personas import Big5PersonaBuilder as Big5PersonaBuilder
 from .prompts import PromptBuilder as PromptBuilder
 
 __all__ = [
     "LLMClient",
     "OpenAIClient",
     "TransformersClient",
+    "PersonaBuilder",
+    "Big5PersonaBuilder",
     "PromptBuilder",
     "DEFAULT_ACTION_JSON_SCHEMA",
     "DEFAULT_ACTION_DESCRIPTION",
