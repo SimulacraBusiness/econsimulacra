b8d02286230694a3b09fb3a5cff2a78d9aba3379
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-03-12 10:40:37 +0900

    enforce int for order/proposal_id

--- src/econsimulacra/envs/order.py
+++ src/econsimulacra/envs/order.py
@@ -9,7 +9,7 @@ class Order:
         item_name: str,
         item_amount: float | int,
         price: Optional[float],
-        order_id: Optional[int] = None,
+        order_id: int,
         ttl: Optional[int] = None,
     ) -> None:
         self.agent_id: int = agent_id
@@ -17,7 +17,7 @@ class Order:
         self.item_name: str = item_name
         self.item_amount: float | int = item_amount
         self.price: Optional[float] = price
-        self.order_id: Optional[int] = order_id
+        self.order_id: int = order_id
         self.expire_in: int = ttl if ttl is not None else 2
         self.accepted_amount: float | int = 0
 
@@ -55,7 +55,7 @@ class SwapProposal:
         give_item_amount: float | int,
         get_item_name: str,
         get_item_amount: float | int,
-        proposal_id: Optional[int] = None,
+        proposal_id: int,
         ttl: Optional[int] = None,
     ) -> None:
         self.proposer_agent_id: int = proposer_agent_id
@@ -64,7 +64,7 @@ class SwapProposal:
         self.give_item_amount: float | int = give_item_amount
         self.get_item_name: str = get_item_name
         self.get_item_amount: float | int = get_item_amount
-        self.proposal_id: Optional[int] = proposal_id
+        self.proposal_id: int = proposal_id
         self.expire_in: int = ttl if ttl is not None else 1
         self.accept: Optional[bool] = None
 
