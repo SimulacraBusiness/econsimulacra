d04424dc4cce3469aa0e0ab5ee4363d653443153
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-04-22 19:54:53 +0900

    format

--- src/econsimulacra/__init__.py
+++ src/econsimulacra/__init__.py
@@ -1,4 +1,12 @@
-from econsimulacra import agents as agents, envs as envs, items as items, logs as logs
+from econsimulacra import (
+    agents as agents,
+    envs as envs,
+    items as items,
+    llm_services as llm_services,
+    logs as logs,
+    memory as memory,
+    social_networks as social_networks,
+)
 from econsimulacra.sim_utils import JsonRandom as JsonRandom, find_class as find_class
 from econsimulacra.simulator import (
     SimulationSummarizer as SimulationSummarizer,
@@ -9,7 +17,10 @@ __all__ = [
     "agents",
     "envs",
     "items",
+    "llm_services",
     "logs",
+    "memory",
+    "social_networks",
     "Simulator",
     "find_class",
     "JsonRandom",
