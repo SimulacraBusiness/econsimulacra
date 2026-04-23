1babe296b3cf05a75797e5b837d2b70507a176d1
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-04-23 18:11:42 +0900

    apply get_description

--- src/econsimulacra/llm_services/prompts/base.py
+++ src/econsimulacra/llm_services/prompts/base.py
@@ -9,6 +9,7 @@ from ..constant import (
     DEFAULT_OBS_DESCRIPTION,
     DEFAULT_SIMULATION_DESCRIPTION,
 )
+from ..llm_utils import get_description
 
 
 class PromptBuilder:
@@ -118,26 +119,14 @@ class PromptBuilder:
                 - econsimulacra.constant.DEFAULT_OBS_DESCRIPTION
                 - econsimulacra.constant.DEFAULT_ACTION_DESCRIPTION, respectively.
         """
-        obs_desc_path_str: Optional[str] = config.get("obsDescriptionPath")
-        obs_desc: str
-        if obs_desc_path_str is not None:
-            obs_desc_path: Path = pathlib.Path(obs_desc_path_str).resolve()
-            if not obs_desc_path.exists():
-                raise FileNotFoundError(
-                    f"Observation description file not found at: {obs_desc_path}"
-                )
-            obs_desc = obs_desc_path.read_text(encoding="utf-8")
-        else:
-            obs_desc = json.dumps(DEFAULT_OBS_DESCRIPTION, ensure_ascii=False)
-        action_desc_path_str: Optional[str] = config.get("actionDescriptionPath")
-        action_desc: str
-        if action_desc_path_str is not None:
-            action_desc_path: Path = pathlib.Path(action_desc_path_str).resolve()
-            if not action_desc_path.exists():
-                raise FileNotFoundError(
-                    f"Action description file not found at: {action_desc_path}"
-                )
-            action_desc = action_desc_path.read_text(encoding="utf-8")
-        else:
-            action_desc = json.dumps(DEFAULT_ACTION_DESCRIPTION, ensure_ascii=False)
+        obs_desc: str = get_description(
+            path_str=config.get("obsDescriptionPath"),
+            default_description=json.dumps(DEFAULT_OBS_DESCRIPTION, ensure_ascii=False),
+        )
+        action_desc: str = get_description(
+            path_str=config.get("actionDescriptionPath"),
+            default_description=json.dumps(
+                DEFAULT_ACTION_DESCRIPTION, ensure_ascii=False
+            ),
+        )
         return obs_desc, action_desc
