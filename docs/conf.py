324ac48152b7af0a713114d622ca1e39fa4f39f9
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-05-12 10:54:14 +0900

    add matplotlib

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 project = "EconSimulacra"
 copyright = "2026, Simulacra Inc"
 author = "Ryuji Hashimoto"
-release = "0.9.0"
+release = "0.11.4"
 
 # -- General configuration -----------------------------------------------------
 extensions = [
@@ -41,6 +41,7 @@ autodoc_mock_imports = [
     "rich",
     "accelerate",
     "datasets",
+    "matplotlib",
     "transformers",
     "statsmodels",
     "sentencepiece",
