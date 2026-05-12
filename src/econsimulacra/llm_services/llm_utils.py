a4ceb4b59758954d4520cef446e5c6e898e8b397
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-05-12 14:50:27 +0900

    change types and add remove_keys

--- src/econsimulacra/llm_services/llm_utils.py
+++ src/econsimulacra/llm_services/llm_utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import json
 import pathlib
 from pathlib import Path
 from typing import Optional
@@ -7,15 +8,18 @@ from typing import Optional
 
 def get_description(
     path_str: Optional[str],
-    default_description: str,
+    default_description: dict[str, str],
+    remove_keys: Optional[list[str]] = None,
 ) -> str:
     """Get description from a given file path or use the default description.
 
     Args:
         path_str (str, optional): The file path to the description text file.
             If None, the default description will be used.
-        default_description (str): The default description to use if path_str is None.
-
+        default_description (dict[str, str]): The default description to use if path_str is None.
+        remove_keys (list[str], optional): A list of keys to remove from the description dictionary.
+            If None, no keys will be removed.
+        
     Returns:
         str: The description obtained from the file or the default description.
 
@@ -24,12 +28,17 @@ def get_description(
             econsimulacra.llm_services.PromptBuilder._get_obs_action_description()
             econsimulacra.llm_services.PersonaBuilder._get_persona_description()
     """
-    desc: str
+    desc_dic: dict[str, str]
     if path_str is not None:
         desc_path: Path = pathlib.Path(path_str).resolve()
         if not desc_path.exists():
             raise FileNotFoundError(f"Description file not found at: {desc_path}")
-        desc = desc_path.read_text(encoding="utf-8")
+        desc_dic = json.loads(desc_path.read_text(encoding="utf-8"))  # type: ignore
     else:
-        desc = default_description
+        desc_dic = default_description
+    if remove_keys is not None:
+        for key in remove_keys:
+            if key in desc_dic:
+                del desc_dic[key]
+    desc: str = json.dumps(desc_dic, ensure_ascii=False)
     return desc
