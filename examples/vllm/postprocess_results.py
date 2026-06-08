02c12863559196c3b57e874eae243399050c3e59
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-06-08 09:15:41 +0900

    change directry

--- examples/vllm/postprocess_results.py
+++ examples/vllm/postprocess_results.py
@@ -3,6 +3,5 @@ from econsimulacra.log_postprocess import build_sentiment_processor
 if __name__ == "__main__":
     processor = build_sentiment_processor()
     processor.process_file(
-        input_file="log_gpt-oss-120b_42.txt",
-        output_file="log_gpt-oss-120b_42_with_sentiment.txt",
+        "log_gpt-oss-120b/42.txt", "log_gpt-oss-120b_42_processed.txt"
     )
