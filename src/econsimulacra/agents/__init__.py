e15b40100131227e9f0088192647e1f8b8557f59
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-08-28 10:28:28 +0900

    add RuleBasedHousehold

--- src/econsimulacra/agents/__init__.py
+++ src/econsimulacra/agents/__init__.py
@@ -1,6 +1,15 @@
 from .auto_reacter import AutoReactLLMAgent as AutoReactLLMAgent
 from .base import Agent as Agent
 from .government import Government as Government
+from .households import RuleBasedHousehold as RuleBasedHousehold
 from .llm_agent import LLMAgent as LLMAgent
+from .retailer import RuleBasedRetailer as RuleBasedRetailer
 
-__all__ = ["Agent", "LLMAgent", "AutoReactLLMAgent", "Government"]
+__all__ = [
+    "Agent",
+    "LLMAgent",
+    "AutoReactLLMAgent",
+    "Government",
+    "RuleBasedHousehold",
+    "RuleBasedRetailer"
+]
