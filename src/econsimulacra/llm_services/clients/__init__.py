7cb2edfe18472adcc9dc94253e9a0b9da6ee463d
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-08-29 15:46:20 +0900

    add text generators

--- src/econsimulacra/llm_services/clients/__init__.py
+++ src/econsimulacra/llm_services/clients/__init__.py
@@ -4,14 +4,18 @@ from .llm_client_utils import (
     save_response_record_from_chat_completion as save_response_record_from_chat,
 )
 from .openai_client import OpenAIClient as OpenAIClient
+from .text_base import TextGenerationClient as TextGenerationClient
 from .transformers_client import TransformersClient as TransformersClient
+from .transformers_text_client import TransformersTextClient as TransformersTextClient
 from .vllm_client import VLLMClient as VLLMClient
 
 __all__ = [
     "LLMClient",
     "LLMRecordConfig",
     "OpenAIClient",
+    "TextGenerationClient",
     "TransformersClient",
+    "TransformersTextClient",
     "VLLMClient",
     "modify_schema",
     "save_response_record_from_chat",
