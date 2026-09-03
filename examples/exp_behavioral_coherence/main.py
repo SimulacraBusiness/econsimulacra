dd5f145ed435854a717445976ebd1c1e52e3d827
hashimoto-ryuji0110 <hashimoto-ryuji419@g.ecc.u-tokyo.ac.jp>
2026-09-03 14:54:37 +0900

    baseline llm

--- examples/exp_behavioral_coherence/main.py
+++ examples/exp_behavioral_coherence/main.py
@@ -10,24 +10,25 @@ from econsimulacra.envs import Environment
 from econsimulacra.logs import DictLogger
 from econsimulacra.simulator import SimulationSummarizer, Simulator
 
-# export LOG_TXT_PATH="log_baseline.txt"
 
 
 def conduct_simulation():
-    config_dic_path: Path = pathlib.Path(__file__).parent / "config_baseline.json"
-    logger: DictLogger = DictLogger()
-    simulator: Simulator = Simulator(
-        config=config_dic_path,
-        env_class=Environment,
-        logger=logger,
-        summarizer_class=SimulationSummarizer,
-    )
-    asyncio.run(simulator.simulate(seed=42))
-    logs: list[dict] = logger.logs
-    log_txt_path: Path = pathlib.Path(os.environ["LOG_TXT_PATH"])
-    with open(log_txt_path, "w", encoding="utf-8") as f:
-        for log in logs:
-            f.write(json.dumps(log, ensure_ascii=False) + "\n")
+    config_dic_path: Path = pathlib.Path(__file__).parent / "config_baseline_llm.json"
+    for seed in range(42, 52):
+        logger: DictLogger = DictLogger()
+        simulator: Simulator = Simulator(
+            config=config_dic_path,
+            env_class=Environment,
+            logger=logger,
+            summarizer_class=SimulationSummarizer,
+        )
+        asyncio.run(simulator.simulate(seed=seed))
+        logs: list[dict] = logger.logs
+        llm_type: str = "gpt-oss-20b"
+        log_txt_path: Path = pathlib.Path(f"logs/baseline/{llm_type}/{seed}.txt")
+        with open(log_txt_path, "w", encoding="utf-8") as f:
+            for log in logs:
+                f.write(json.dumps(log, ensure_ascii=False) + "\n")
 
 
 if __name__ == "__main__":
